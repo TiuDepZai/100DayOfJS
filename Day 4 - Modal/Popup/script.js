d06eb2a519e816ffdc499e9d74689286afc5de59
TiuDepZai <58964817+TiuDepZai@users.noreply.github.com>
2024-02-25 00:04:28 +1000

    Updated Day 4

--- Day 4 - Modal/Popup/script.js
+++ Day 4 - Modal/Popup/script.js
@@ -1,3 +1,20 @@
 // variables
 const modal = document.querySelector(".modal"),
-    btn = document.querySelector("btn")
+    btn = document.querySelector("btn"),
+    close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// Open Modal
+function openModal(e){
+    e.prevemtDefaul();
+    modal.style.display = "block";
+}
+
+// Close Modal
+function closeModal(){
+    
+    modal.style.display = "nones";
+}
